9ecb77867695df340c844d085ce6040e6bf758e7
Naman Sharma <naman99sha@gmail.com>
2023-05-13 09:05:28 +0530

    Create View and URL for template based list of Movies in tabular form

--- ImdbProjectCsv/MovieDataApp/views.py
+++ ImdbProjectCsv/MovieDataApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import MovieData, Genre
 
-# Create your views here.
+def listAllMoviesOnTemplate(request):
+    all_movies = MovieData.objects.all().order_by('id')
+    return render(request,'MovieTable.html',{'all_movies':all_movies})
